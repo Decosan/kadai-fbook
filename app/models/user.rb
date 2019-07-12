58e4444f7d3189e55c5c3ca3b2481e6e1445f505
Decosan <junx0401@gmail.com>
2019-07-12 15:18:32 +0900

    ブートストラップ設定

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,8 +1,8 @@
 class User < ApplicationRecord
   has_secure_password
   before_validation {email.downcase!}
-  validates :name, :presence true,length:{maximum: 25}
-  validates :email, :presence true,length:{maximum: 25},
+  validates :name,presence: true,length:{maximum: 25}
+  validates :email,presence: true,length:{maximum: 25},
                     format: { with: /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i },
                     uniqueness: true
 end
