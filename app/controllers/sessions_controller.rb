c7ed4030910657c2850bfe54f539f0e9ea5c4350
Decosan <junx0401@gmail.com>
2019-07-12 16:20:17 +0900

    ログインページ作成

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,5 +1,18 @@
 class SessionsController < ApplicationController
+
+  def new
+  end
+
   def create
+    user=User.find_by(email: params[:session][:email].downcase)
+    if user && user.authenticate(params[:session][:password])
+      session[:user_id]=user.id
+      flash[:success]="logged in!!"
+      redirect_to users_path
+    else
+      flash[:danger]="Failed.."
+      render 'new'
+    end
   end
 
   def destroy
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
 
-  resources :sessions, only:[:create,:destroy]
+  resources :sessions, only:[:new,:create,:destroy]
   resources :users, only:[:index,:show,:new,:create]
 
 end
