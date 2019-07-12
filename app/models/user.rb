e81a33ff756ce85e64800dda6c0851ac732e4c54
Decosan <junx0401@gmail.com>
2019-07-12 17:07:19 +0900

    ポストモデル、ポストコントローラー作成

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,6 @@
 class User < ApplicationRecord
   has_secure_password
+  has_many :posts
   before_validation {email.downcase!}
   validates :name,presence: true,length:{maximum: 25}
   validates :email,presence: true,length:{maximum: 25},
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,12 @@
 Rails.application.routes.draw do
 
+  get 'posts/index'
+  get 'posts/show'
+  get 'posts/new'
+  get 'posts/create'
+  get 'posts/edit'
+  get 'posts/update'
+  get 'posts/destroy'
   resources :sessions, only:[:new,:create,:destroy]
   resources :users, only:[:index,:show,:new,:create]
 
