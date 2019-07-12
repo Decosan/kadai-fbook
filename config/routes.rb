0dbafd024ae8afd925ac0b74803d0f47f533b3d3
Decosan <junx0401@gmail.com>
2019-07-12 15:41:18 +0900

    user index page　作成

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,5 @@
 Rails.application.routes.draw do
-  get 'users/index'
-  get 'users/show'
-  get 'users/new'
-  get 'users/create'
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+  resources :users, only:[:index,:show,:new,:create]
+
 end
