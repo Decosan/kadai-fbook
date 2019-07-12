694d305ad9c22267ac5b4bd17bf62b65688b67eb
Decosan <junx0401@gmail.com>
2019-07-12 15:57:23 +0900

    user show new create page finish

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -8,8 +8,23 @@ class UsersController < ApplicationController
   end
 
   def new
+    @user=User.new
   end
 
   def create
+    @user=User.new(user_params)
+    if @user.save
+      flash[:success]="Sign up!"
+      redirect_to users_path
+    else
+      flash.now[:danger]="Failed.."
+      render 'new'
+    end
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:name,:email,:password,:password_confirmation)
   end
 end
