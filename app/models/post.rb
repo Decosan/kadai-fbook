685d3292772c96f189ff1fe518406894388e0e81
Decosan <junx0401@gmail.com>
2019-07-13 09:08:30 +0900

    中間テーブルFavorite作成

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,5 +1,7 @@
 class Post < ApplicationRecord
   belongs_to :user
+  has_many :favorites, dependent: :destroy
+  has_many :favorite_users,through: :favorites,source: :user
   validates :title,presence: true,length:{maximum: 25}
   validates :content,presence: true,length:{maximum: 255}
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,7 @@
 class User < ApplicationRecord
   has_secure_password
   has_many :posts
+  has_many :favorites, dependent: :destroy
   before_validation {email.downcase!}
   validates :name,presence: true,length:{maximum: 25}
   validates :email,presence: true,length:{maximum: 25},
