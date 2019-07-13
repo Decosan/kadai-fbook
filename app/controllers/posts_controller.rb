95209a869758e46655073445846d3ee3b3ccfc00
Decosan <junx0401@gmail.com>
2019-07-13 09:03:36 +0900

    ポストのnew,edit,confirm,destroyの設定

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -5,23 +5,62 @@ class PostsController < ApplicationController
     @posts=Post.all.order('updated_at DESC')
   end
 
+  def confirm
+    @post=Post.new(post_params)
+    if @post.invalid?
+      render :new
+    end
+  end
+
   def show
     @post=Post.find(params[:id])
   end
 
   def new
-    
+    if params[:back]
+      @post=Post.new(post_params)
+    else
+      @post=Post.new
+    end
   end
 
   def create
+    @post=Post.new(post_params)
+    @post.user_id=current_user.id
+    if @post.save
+      flash[:success]='Posted!!'
+      redirect_to posts_path
+    else
+      flash.now[:danger]='Failed..'
+      render 'new'
+    end
   end
 
   def edit
+    @post=Post.find(params[:id])
   end
 
   def update
+    @post=Post.find(params[:id])
+    if @post.update(post_params)
+      flash[:success]='Edit completed'
+      redirect_to posts_path
+    else
+      flash[:danger]='Failed..'
+      render 'edit'
+    end
   end
 
   def destroy
+    @post=Post.find(params[:id])
+    @post.destroy
+    flash[:danger]='Deleted!'
+    redirect_to posts_path
+  end
+
+  private
+
+  def post_params
+    params.require(:post).permit(:title,:content,:user)
   end
 end
